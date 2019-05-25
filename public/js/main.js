94c4fbecfd36a2f540354bae213122df5398b6b0
Vadim <vadim.a.shesterikov@gmail.com>
2019-05-25 14:00:51 +0300

    Added search function

--- public/js/main.js
+++ public/js/main.js
@@ -26,3 +26,23 @@ function remove(id) {
   }
   document.getElementById('t' + String(id)).remove()
 }
+
+function srch() {
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("search");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("tt");
+  tr = table.getElementsByTagName("tr");
+  
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("th")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    } 
+  }
+}
