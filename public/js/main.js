c3d63cf5eabd042eb7b3f02c5aa5918036b6dfea
Vadim <vadim.a.shesterikov@gmail.com>
2019-05-26 20:14:40 +0300

    Use fetch

--- public/js/main.js
+++ public/js/main.js
@@ -1,34 +1,23 @@
 let d = 0;
-
 function check(id, done) {
-  console.log(done)
   if(d % 2) {
     done = (done == "true" ? "false" : "true")
-    console.log(done + " " + String(d))
   } d++
-  const Http = new XMLHttpRequest();
-  const url="/edit/" + id + "/" + done;
-  Http.open("POST", url);
-  Http.send();
-  Http.onreadystatechange=(e)=>{
-    console.log(Http.responseText)
-  }
-
+  fetch("/edit/" + id + "/" + done, {
+      method: 'POST'
+    });
 }
 
 function remove(id) {
-  const Http = new XMLHttpRequest();
-  const url="/delete/" + String(id);
-  Http.open("POST", url);
-  Http.send();
-  Http.onreadystatechange=(e)=>{
-    console.log(Http.responseText)
-  }
-  document.getElementById('t' + String(id)).remove()
+  fetch("/delete/" + String(id), {
+    method: 'POST'
+  }).then(function() {
+    document.getElementById('t' + String(id)).remove()
+  });
 }
 
 function srch() {
-  var input, filter, table, tr, th, i, txtValue;
+  let input, filter, table, tr, th, i, txtValue;
   input = document.getElementById("search");
   filter = input.value.toUpperCase();
   table = document.getElementById("tt");
