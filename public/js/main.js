d2fcd080801e5b0349ea8737aa635e2bf5aff741
Vadim <vadim.a.shesterikov@gmail.com>
2019-05-26 19:47:42 +0300

    Fix search

--- public/js/main.js
+++ public/js/main.js
@@ -28,16 +28,16 @@ function remove(id) {
 }
 
 function srch() {
-  var input, filter, table, tr, td, i, txtValue;
+  var input, filter, table, tr, th, i, txtValue;
   input = document.getElementById("search");
   filter = input.value.toUpperCase();
   table = document.getElementById("tt");
   tr = table.getElementsByTagName("tr");
 
-  for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("th")[0];
-    if (td) {
-      txtValue = td.textContent || td.innerText;
+  for (i = 1; i < tr.length; i++) {
+    th = tr[i].getElementsByTagName("th")[0];
+    if (th) {
+      txtValue = th.textContent || th.innerText;
       if (txtValue.toUpperCase().indexOf(filter) > -1) {
         tr[i].style.display = "";
       } else {
